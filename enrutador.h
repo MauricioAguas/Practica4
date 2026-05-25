cb5effcdefa5b0e417e2756fe11ca0cced9e1173
MauricioAguas <62766009+MauricioAguas@users.noreply.github.com>
2026-05-25 11:47:48 -0500

    Clase Enrutador con atributos básicos y vecinos

--- enrutador.h
+++ enrutador.h
@@ -2,8 +2,7 @@
 #define ENRUTADOR_H
 
 #include <string>
-
-// agregar librerias faltantes...
+#include <map>
 
 using namespace std;
 
@@ -12,20 +11,22 @@ class Enrutador
 {
 private:
     string nombre;
-    // Elegir contenedor adecuado para guardar
-    // enlaces directos a otros enrutadores y su costo
-    ... enlaces;
+    // map<destino, costo> para guardar enlaces directos a otros enrutadores
+    map<string, int> enlaces;
 
 public:
     // Constructores y destructor
     Enrutador()
         : nombre("")
     {}              // Constructor por defecto
+    Enrutador(const string &nombre)
+        : nombre(nombre)
+    {}              // Constructor con nombre
     ~Enrutador() {} // Destructor
 
     // Getters
-    const string &getNombre() const { return nombre; } // Getter para el nombre
-    const... &getEnlaces() const { return enlaces; }   // Getter para los enlaces
+    const string &getNombre() const { return nombre; }          // Getter para el nombre
+    const map<string, int> &getEnlaces() const { return enlaces; } // Getter para los enlaces
 
     // Métodos de funcionamiento para gestionar los enlaces
     void agregarEnlace(const string &destino, int costo);
